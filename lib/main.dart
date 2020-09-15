5ec5afb5598b6904610ce926d780c92c8fb09067
Saurav Kumar <saurav0001kumar@gmail.com>
2020-09-15 17:02:22 +0530

    statusBarColor issue Fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './Drawer/drawer.dart';
 void main() {
-  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-  //   statusBarColor: Colors.transparent,
-  // ));
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white,
+    statusBarIconBrightness: Brightness.dark,
+  ));
   runApp(MyApp());
 }
 
